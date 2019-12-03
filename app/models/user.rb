127bb1034e287b865a03b83dab5a21a06069ca1d
Tonino93 <anthonyl1993@laposte.net>
2019-12-04 00:13:52 +0100

    ViewUserDevise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :childrens, dependent: :destroy
+  has_many :children, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
