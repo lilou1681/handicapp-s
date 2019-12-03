f06c771f19cefb6826a51c9d5f896284f7fe1fa7
Tonino93 <anthonyl1993@laposte.net>
2019-12-03 12:45:01 +0100

    Model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :childrens, dependent: :destroy
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :address, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
