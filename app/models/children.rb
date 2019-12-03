f534374557ac73030e413afa44fa655f342c65d4
Helene Petitjean <leloo8@msn.com>
2019-12-03 12:56:21 +0100

    validates children

--- app/models/children.rb
+++ app/models/children.rb
@@ -1,3 +1,12 @@
 class Children < ApplicationRecord
   belongs_to :user
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :birth_day, presence: true
+    validates :gender, presence: true
+    validates :nationality, presence: true
+    validates :country, presence: true
+    validates :address, presence: true
+    validates :security_number, uniqueness: true, presence: true
+    validates :life_project, presence: true
 end
