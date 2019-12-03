13186893db4ba9b660cae246ffb3f69a7d57cc4f
Tonino93 <anthonyl1993@laposte.net>
2019-12-03 17:59:14 +0100

    SeedAnthony

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,24 +23,26 @@ puts "Start Seeding ..."
     last_name: Faker::Name.last_name,
     password: "password",
     password_confirmation: "password",
-    address: Faker::Address.street_address
+    address: Faker::Address.street_address,
+    remote_photo_url: "https://picsum.photos/id/80/200/300"
   )
   children = Children.create!(
     user_id: user.id,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    birth_day:Faker::Date.birthday(min_age: 0, max_age: 65) ,
+    birth_day: Faker::Date.between(from: 2.days.ago, to: Date.today),
     gender: Faker::Gender.binary_type,
-    nationality:Faker::Nation.nationality,
+    nationality: Faker::Nation.nationality,
     country: Faker::Address.city,
     address: Faker::Address.street_address,
-    security_number:Faker::Number.number(digits: 15),
-    life_project: Faker::Lorem.paragraph  ,
+    security_number: Faker::Number.number(digits: 15),
+    life_project: Faker::Lorem.paragraph,
+    remote_photo_url: "https://picsum.photos/200/300"
   )
     reminder = Reminder.create!(
       children_id: children.id,
       name: Faker::Name,
-      date: Faker::date,
+      date: Faker::Date.between(from: 2.days.ago, to: Date.today)
       )
 end
 puts "#{User.count} users created"
