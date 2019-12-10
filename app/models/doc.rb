911a8b0f64e7036034c84a7e54771f52640c05aa
Helene Petitjean <leloo8@msn.com>
2019-12-10 14:05:16 +0100

    displaypdf

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,5 +1,8 @@
 class Doc < ApplicationRecord
   belongs_to :children
-
   mount_uploader :file, FileUploader
+
+  NAMES = ["CERFA MDPH", "CERFA Certificat médical", "Projet de vie"]
+
+  validates :name, presence: true, inclusion: { in: NAMES }
 end
