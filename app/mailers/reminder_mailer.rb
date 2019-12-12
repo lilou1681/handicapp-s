ef0cea12aabe20416aa513586382a23412ef116a
Helene Petitjean <leloo8@msn.com>
2019-12-12 11:11:22 +0100

    new email address

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,5 +1,5 @@
 class ReminderMailer < ApplicationMailer
-  default from: "handicapps1681@gmail.com"
+  default from: "handicapps2019@gmail.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
